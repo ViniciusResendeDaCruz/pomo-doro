00f259c2f8ec3f792ce655b479e5206714ffa707
ViniciusResendeDaCruz <vinicius.resende.cruz@gmail.com>
2025-11-19 15:40:35 -0300

    Adiciona a página Info com explicações sobre a técnica PomoDoro e envolve os componentes em um Container

---
+++ src/pages/Info/index.tsx
@@ -0,0 +1,44 @@
+import { MainTemplate } from '../../templates/MainTemplate';
+import { Container } from '../../components/Container';
+export function Info() {
+  return (
+    <MainTemplate>
+      <Container>
+        <h1 className='text-primary fs-1 fw-bold mb-2'>
+          O que é a técnica PomoDoro?
+        </h1>
+        <p className='text-neutral'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum
+          labore cumque quisquam consectetur possimus eaque iste, consequuntur
+          aliquid repellendus similique ea vel quae odit fugit. Eligendi
+          mollitia quo suscipit modi. Ipsa, reiciendis nostrum voluptatibus
+          beatae saepe ea sit molestiae illo recusandae deserunt dolores iste
+          esse ipsum vitae vero quis autem labore. Deleniti excepturi nulla
+          dicta. Veritatis deleniti eum temporibus ad saepe rerum amet
+          blanditiis iusto placeat praesentium! Iste necessitatibus sit enim
+          minima repudiandae! Necessitatibus doloribus consectetur ut asperiores
+          velit reprehenderit porro fugit qui sequi minima exercitationem odit
+          est saepe id, placeat quos perspiciatis. Iure voluptatem odit vero
+          officia minus nostrum!
+        </p>
+        <h1 className='text-primary fs-1 fw-bold mb-2'>
+          Como funciona a técnica PomoDoro?
+        </h1>
+        <p className='text-neutral'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum
+          labore cumque quisquam consectetur possimus eaque iste, consequuntur
+          aliquid repellendus similique ea vel quae odit fugit. Eligendi
+          mollitia quo suscipit modi. Ipsa, reiciendis nostrum voluptatibus
+          beatae saepe ea sit molestiae illo recusandae deserunt dolores iste
+          esse ipsum vitae vero quis autem labore. Deleniti excepturi nulla
+          dicta. Veritatis deleniti eum temporibus ad saepe rerum amet
+          blanditiis iusto placeat praesentium! Iste necessitatibus sit enim
+          minima repudiandae! Necessitatibus doloribus consectetur ut asperiores
+          velit reprehenderit porro fugit qui sequi minima exercitationem odit
+          est saepe id, placeat quos perspiciatis. Iure voluptatem odit vero
+          officia minus nostrum!
+        </p>
+      </Container>
+    </MainTemplate>
+  );
+}
