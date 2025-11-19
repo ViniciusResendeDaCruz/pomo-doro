d12dbe30448523358961bd728afa128e72c61d7c
ViniciusResendeDaCruz <vinicius.resende.cruz@gmail.com>
2025-11-19 10:35:41 -0300

    Cria o CountDown para contagem de tempo

---
+++ src/components/CountDown/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+// import { Icon } from '@iconify/react';
+
+export function CountDown() {
+  
+  return (
+    <div className={`${styles.number}`}>00:00</div>
+  );
+}
