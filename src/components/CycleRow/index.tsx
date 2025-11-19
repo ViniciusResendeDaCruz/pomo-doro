c0cfe706938f38cc9e8c0742d7dd25c91089f54b
ViniciusResendeDaCruz <vinicius.resende.cruz@gmail.com>
2025-11-19 10:35:27 -0300

    Separa o contador de ciclos

---
+++ src/components/CycleRow/index.tsx
@@ -0,0 +1,13 @@
+// import styles from './styles.module.css';
+// import { Icon } from '@iconify/react';
+
+export function CycleRow() {
+  return (
+    <div className='d-flex flex-column align-items-center mt-4'>
+      <span className='fs-6 text-center'>Ciclos</span>
+      <div className='cycleDots'>
+        <p>0 0 0 0 0 0 0</p>
+      </div>
+    </div>
+  );
+}
