541a2c050cd92ef9b63a480c88428224406c44a6
ViniciusResendeDaCruz <vinicius.resende.cruz@gmail.com>
2025-11-19 10:45:23 -0300

    Ano dinamico no Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,10 +2,11 @@
 // import { Icon } from '@iconify/react';
 
 export function Footer() {
+  const year = new Date().getFullYear();
   return (
     <footer className='d-flex flex-row justify-content-center align-items-center fixed-bottom border-top border-dark py-4'>
       <span className='fs-6 text-center'>
-       <strong>Pomo-Doro</strong> © 2025 - Desenvolvido com 💜 por <a href='https://github.com/ViniciusResendeDaCruz' target='_blank' rel='noopener noreferrer'>Vincius Resende</a>
+       <strong>Pomo-Doro</strong> © {year} - Desenvolvido com 💜 por <a href='https://github.com/ViniciusResendeDaCruz' target='_blank' rel='noopener noreferrer'>Vincius Resende</a>
       </span>
       {/* <span className='fs-6 text-center'>Desenvolvido por <a href='https://github.com/ViniciusResendeDaCruz' target='_blank' rel='noopener noreferrer'>Vincius Resende</a></span> */}
     </footer>
