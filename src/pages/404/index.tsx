590290e281a3369c7ec2f28fbcbbce5079ae8293
ViniciusResendeDaCruz <vinicius.resende.cruz@gmail.com>
2025-11-19 14:02:57 -0300

    Adiciona página 404 personalizada com mensagem de erro e template principal

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,9 @@
+import { MainTemplate } from '../../templates/MainTemplate';
+ 
+export function NotFound() {
+  return (
+    <MainTemplate>
+        <p>A página não foi encontrada</p>
+    </MainTemplate>
+  );
+}
