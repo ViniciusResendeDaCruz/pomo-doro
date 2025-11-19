d3e5f712bf83c6046e7860545064e6625a616846
ViniciusResendeDaCruz <vinicius.resende.cruz@gmail.com>
2025-11-19 10:36:25 -0300

    Atualiza o App para refletir todas as alterações de elementos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import './styles/styles.css';
 import './styles/global.css';
-import { HelloWorld } from './components/HelloWorld';
-
+// import { HelloWorld } from './components/HelloWorld';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { MainForm } from './components/MainForm';
+ 
 export function App() {
   console.log('MyApp');
   return (
     <>
-      <HelloWorld>Mundinho bão!</HelloWorld>
+      <div className='d-flex flex-column align-items-center vh-100 w-100 p-4'>
+        <Logo />
+        {/* <HelloWorld>Mundo</HelloWorld> */}
+        <Menu />
+        <CountDown />
+        <MainForm />
+      </div>
     </>
   );
 }
