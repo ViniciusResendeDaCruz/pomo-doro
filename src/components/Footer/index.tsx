4c63589589821b617ef0e3a4255fab933a2e145d
ViniciusResendeDaCruz <vinicius.resende.cruz@gmail.com>
2025-11-19 10:36:06 -0300

    Cria o footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+// import styles from './styles.module.css';
+// import { Icon } from '@iconify/react';
+
+export function Footer() {
+  return (
+    <footer className='d-flex flex-row justify-content-center align-items-center'>
+      <span className='fs-6 text-center'>Footer</span>
+    </footer>
+  );
+}
