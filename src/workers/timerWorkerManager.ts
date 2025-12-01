e16bc9109f36fafef10dc582ee23ed3bc71a2f85
ViniciusResendeDaCruz <vinicius.resende.cruz@gmail.com>
2025-12-01 10:39:04 -0300

    Refatora TimerWorkerManager para melhorar a legibilidade e adiciona tipagem ao método postMessage

--- src/workers/timerWorkerManager.ts
+++ src/workers/timerWorkerManager.ts
@@ -1,29 +1,31 @@
+import type { TaskStateModel } from '../models/TaskStateModel';
+
 let instance: TimerWorkerManager | null = null;
 
 export class TimerWorkerManager {
-    private worker: Worker;
+  private worker: Worker;
 
-    private constructor() {
-        this.worker = new Worker(new URL('./timerWorker.js', import.meta.url));
-    }
+  private constructor() {
+    this.worker = new Worker(new URL('./timerWorker.js', import.meta.url));
+  }
 
-    public static getInstance(): TimerWorkerManager {
-        if (!instance) {
-            instance = new TimerWorkerManager();
-        }
-        return instance;
+  public static getInstance(): TimerWorkerManager {
+    if (!instance) {
+      instance = new TimerWorkerManager();
     }
+    return instance;
+  }
 
-    public postMessage(message: any) {
-        this.worker.postMessage(message);
-    }
+  public postMessage(message: TaskStateModel) {
+    this.worker.postMessage(message);
+  }
 
-    public onmessage(cb: (e: MessageEvent) => void): void {
-        this.worker.onmessage = cb;
-    }
+  public onmessage(cb: (e: MessageEvent) => void): void {
+    this.worker.onmessage = cb;
+  }
 
-    public terminate(): void {
-        this.worker.terminate();
-        instance = null;
-    }
+  public terminate(): void {
+    this.worker.terminate();
+    instance = null;
+  }
 }
